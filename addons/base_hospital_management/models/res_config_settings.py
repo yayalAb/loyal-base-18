1abf788fab5157f956657a497a6417ef7caa91e4
yayal <yayalabayneh2@gmail.com>
2025-11-14 11:28:57 +0300

    update validity

--- addons/base_hospital_management/models/res_config_settings.py
+++ addons/base_hospital_management/models/res_config_settings.py
@@ -5,7 +5,7 @@ class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
     patient_validity_days = fields.Integer(
-        string='Patient Validity Period (Days)',
+        string='Patient Card Validity Period (Days)',
         default=10,
         config_parameter='hospital.patient_validity_days',
         help='Number of days before the patient record or ID expires.'
